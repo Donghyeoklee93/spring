06e08eea2c7705cde0103b06f3c3b502b4d65dc3
Donghyeok (Henry) Lee <119444211+Donghyeoklee93@users.noreply.github.com>
2023-05-25 16:12:54 +0100

    add AOP

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,10 +1,13 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.sql.Time;
 
 @Configuration
 public class SpringConfig {
@@ -33,7 +36,13 @@ public class SpringConfig {
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
 //        return new JpaMemberRepository(em);
-
 //    }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
+
+
 
 }
--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -18,11 +18,21 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
+
     public Long join(Member member) {
 
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+//        long start = System.currentTimeMillis();
+
+//        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+
+//        } finally {
+//            long finish = System.currentTimeMillis();
+//            long timeMs = finish - start;
+//            System.out.println("join = " + timeMs + "ms");
+//        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -34,7 +44,15 @@ public class MemberService {
     }
 
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+//        long start = System.currentTimeMillis();
+//        try{
+            return memberRepository.findAll();
+//        } finally {
+//            long finish = System.currentTimeMillis();
+//            long timeMs = finish - start;
+//            System.out.println("findMembers = " + timeMs + "ms");
+//        }
+
     }
 
 
